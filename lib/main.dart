4d61fd7e6f199864c9c9671d58716ca523c28326
mutastry <mutiaraastri2707@gmail.com>
2024-10-23 03:26:16 -0700

    lanjutan candi
    membuat aplikasi candi

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'package:wisatacandi_astri/data/candi_data.dart';
+import 'package:wisatacandi_astri/screens/detail_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:wisatacandi/screens/detail_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,9 +18,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: DetailScreen(),
+      home: DetailScreen(candi:candiList[0]),
     );
-  }
+  }p
 }
 
 
